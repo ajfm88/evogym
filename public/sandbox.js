6dbca961952ce51fee23cf03ca5c6b9a0d463d83
unknown <ajfoucaultmo@mail.usf.edu>
2023-02-22 19:46:11 -0800

    10. Function types (Signatures).

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,10 +1,21 @@
-"use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
+'use strict';
+// let greet: Function;
+// example 1
+var greet;
+greet = function (name, greeting) {
+  console.log(name + ' says ' + greeting);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
+// example 2
+var calc;
+calc = function (numOne, numTwo, action) {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
 };
-const greetAgain = (user) => {
-    console.log(`${user.name} says hello`);
+// example 3
+var logDetails;
+logDetails = function (ninja) {
+  console.log(ninja.name + ' is ' + ninja.age + ' years old');
 };
