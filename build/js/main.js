7fb08a1ce9d552e8937f4c571c1b4bb04d9ea6fd
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-06 19:17:05 -0800

    02 - Typescript Basic Types - Beginners Lesson 01. TS Terminology. 02. Starter Code. 03. Inference, Implicit & Explicit meanings. 04. TS provides more information. 05. String, Number, and Boolean types. 06. The any type. 07. Function parameters. 08. Union types. 09. When in doubt, check inference. 10. Quick review.

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,15 @@
 'use strict';
-let username = 'Dave';
-console.log(username);
-let a = 12;
-let b = '6';
-let c = 2;
-console.log(a / b);
-console.log(c * b);
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+  return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
-let username = 'Dave';
-console.log(username);
+let myName: string = 'Dave';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
 
-let a: number = 12;
-let b: string = '6';
-let c: number = 2;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
 
-console.log(a / b);
+const sum = (a: number, b: string) => {
+  return a + b;
+};
 
-console.log(c * b);
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
