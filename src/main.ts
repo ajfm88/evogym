dc1e10ced7ca98e6dd5fd04fdd2a3e4eedd5bf31
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-07 22:13:43 -0800

    03 - Typescript Objects, Arrays, Tuples & Enums | Basics Tutorial 01. Array Type Inference. 02. Array Assignment. 03. Array Any Type & Explicit Types. 04. Tuples. 05. Object Type. 06. Object Type Inference. 07. Object Type Annotation. 08. Object Assignment. 09. Optional Object Properties. 10. Object Type as a Parameter. 11. type vs interface. 12. Narrowing with Optional Properties. 13. Enums.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,75 @@
-let myName: string = 'Dave';
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArr = ['one', 'hey', 'Dave']
 
-myName = 'John';
-meaningOfLife = 42;
-isLoading = true;
-album = 5150;
+let guitars = ['Strat', 'Les Paul', 5150]
 
-const sum = (a: number, b: string) => {
-  return a + b;
-};
+let mixedData = ['EVH', 1984, true]
 
-let postId: string | number;
-let isActive: number | boolean;
+stringArr[0] = 'John'
+stringArr.push('hey')
 
-let re: RegExp = /\w+/g;
+guitars[0] = 1984
+guitars.unshift('Jim')
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple 
+let myTuple: [string, number, boolean] = ['Dave', 42, true]
+
+let mixed = ['John', 1, false]
+
+myTuple[1] = 42
+
+// Objects
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
+}
+
+exampleObj.prop1 = 'John'
+
+interface Guitarist {
+    name?: string,
+    active: boolean,
+    albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+}
+
+let jp: Guitarist = {
+    active: true,
+    albums: ['I', 'II', 'IV']
+}
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}!`
+    }
+    return 'Hello!'
+}
+
+console.log(greetGuitarist(jp))
+
+// Enums 
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime."
+
+enum Grade {
+    U = 1,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.U)
