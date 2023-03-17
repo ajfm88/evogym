1d6cb2cded4ca9ae60f9c2817961685ddda38bde
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-17 12:11:18 -0700

    11 - Typescript Project from Scratch | Complete Tutorial for Beginners 01. Challenge 1: ListItem Class. 02. Challenge 2: FullList Class. 03. Challenge 3: ListTemplate Class. 04. Pulling It Together in main.ts. 05. Does It Work?

--- src/main.ts
+++ src/main.ts
@@ -1,137 +1,48 @@
-// Utility Types 
+import './css/style.css'
+import FullList from './model/FullList'
+import ListItem from './model/ListItem'
+import ListTemplate from './templates/ListTemplate'
 
-// Partial 
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
 
-interface Assignment {
-    studentId: string,
-    title: string,
-    grade: number,
-    verified?: boolean,
+  // Add listener to new entry form submit
+  const itemEntryForm = document.getElementById("itemEntryForm") as HTMLFormElement
+
+  itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+    event.preventDefault()
+
+    // Get the new item value
+    const input = document.getElementById("newItem") as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if (!newEntryText.length) return
+
+    // calculate item ID
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1
+
+    // create new item
+    const newItem = new ListItem(itemId.toString(), newEntryText)
+    // Add new item to full list
+    fullList.addItem(newItem)
+    // Re-render list with new item included
+    template.render(fullList)
+  })
+
+  // Add listener to "Clear" button
+  const clearItems = document.getElementById("clearItemsButton") as HTMLButtonElement
+
+  clearItems.addEventListener('click', (): void => {
+    fullList.clearList()
+    template.clear()
+  })
+
+  // load initial data
+  fullList.load()
+  // initial render of template
+  template.render(fullList)
 }
 
-const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
-    return { ...assign, ...propsToUpdate }
-}
-
-const assign1: Assignment = {
-    studentId: "compsci123",
-    title: "Final Project",
-    grade: 0,
-}
-
-console.log(updateAssignment(assign1, { grade: 95 }))
-const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 })
-
-
-// Required and Readonly 
-
-const recordAssignment = (assign: Required<Assignment>): Assignment => {
-    // send to database, etc. 
-    return assign
-}
-
-const assignVerified: Readonly<Assignment> = { ...assignGraded, verified: true }
-
-// NOTE: assignVerified won't work with recordAssignment!
-// Why? Try it and see what TS tells you :)
-
-recordAssignment({ ...assignGraded, verified: true })
-
-// Record 
-const hexColorMap: Record<string, string> = {
-    red: "FF0000",
-    green: "00FF00",
-    blue: "0000FF",
-}
-
-type Students = "Sara" | "Kelly"
-type LetterGrades = "A" | "B" | "C" | "D" | "U"
-
-const finalGrades: Record<Students, LetterGrades> = {
-    Sara: "B",
-    Kelly: "U"
-}
-
-interface Grades {
-    assign1: number,
-    assign2: number,
-}
-
-const gradeData: Record<Students, Grades> = {
-    Sara: { assign1: 85, assign2: 93 },
-    Kelly: { assign1: 76, assign2: 15 },
-}
-
-// Pick and Omit 
-
-type AssignResult = Pick<Assignment, "studentId" | "grade">
-
-const score: AssignResult = {
-    studentId: "k123",
-    grade: 85,
-}
-
-type AssignPreview = Omit<Assignment, "grade" | "verified">
-
-const preview: AssignPreview = {
-    studentId: "k123",
-    title: "Final Project",
-}
-
-// Exclude and Extract 
-
-type adjustedGrade = Exclude<LetterGrades, "U">
-
-type highGrades = Extract<LetterGrades, "A" | "B">
-
-// Nonnullable 
-
-type AllPossibleGrades = 'Dave' | 'John' | null | undefined
-type NamesOnly = NonNullable<AllPossibleGrades>
-
-// ReturnType 
-
-//type newAssign = { title: string, points: number }
-
-const createNewAssign = (title: string, points: number) => {
-    return { title, points }
-}
-
-type NewAssign = ReturnType<typeof createNewAssign>
-
-const tsAssign: NewAssign = createNewAssign("Utility Types", 100)
-console.log(tsAssign)
-
-// Parameters 
-
-type AssignParams = Parameters<typeof createNewAssign>
-
-const assignArgs: AssignParams = ["Generics", 100]
-
-const tsAssign2: NewAssign = createNewAssign(...assignArgs)
-console.log(tsAssign2)
-
-// Awaited - helps us with the ReturnType of a Promise 
-
-interface User {
-    id: number,
-    name: string,
-    username: string,
-    email: string,
-}
-
-const fetchUsers = async (): Promise<User[]> => {
-
-    const data = await fetch(
-        'https://jsonplaceholder.typicode.com/users'
-    ).then(res => {
-        return res.json()
-    }).catch(err => {
-        if (err instanceof Error) console.log(err.message)
-    })
-    return data
-}
-
-type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>
-
-fetchUsers().then(users => console.log(users))
+document.addEventListener("DOMContentLoaded", initApp) 
