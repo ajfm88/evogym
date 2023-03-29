e98ceb55a1c5184d8503c400f52f6f7751d671b3
unknown <ajfoucaultmo@mail.usf.edu>
2023-03-29 12:07:45 -0700

    09. Contact Us Page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
 import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
@@ -30,6 +33,8 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   );
 }
